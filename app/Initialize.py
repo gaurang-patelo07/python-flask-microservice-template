6853b6eca868fd7b5f912ebc2a1a41bf5917c687
Gaurang <86602307+gaurang-patelo07@users.noreply.github.com>
2022-03-07 17:03:32 +0530

    Initialize project structure

---
+++ app/Initialize.py
@@ -0,0 +1,19 @@
+def create_app():
+    pass
+
+
+def create_api():
+    pass
+
+
+def configure_logger():
+    pass
+
+
+def configure_hooks():
+    pass
+
+
+def add_request_id():
+    pass
+
---
+++ app/connector.py
@@ -0,0 +1,15 @@
+def connect_sql():
+    pass
+
+
+def connect_redis():
+    pass
+
+
+def connect_elasticsearch():
+    pass
+
+
+def connect_rabbitmq():
+    pass
+
